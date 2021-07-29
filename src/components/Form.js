e6ce3c21bf126fd96b83473c7274bf4fc448c2bd
William Walsh <wmdwalsh0@gmail.com>
2021-07-29 10:21:35 -0400

    completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
-
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
-
-  return (
+import React, { useState } from 'react';
+function Form(props){
+return(
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+      <input 
+      type="text" 
+      onChange={props.handleFirstNameChange} 
+      value={props.firstName} />
+      <input 
+      type="text" 
+      onChange={props.handleLastNameChange} 
+      value={props.lastName} />
+    <button type="submit">Submit</button>
+  </form> 
   );
 }
 
